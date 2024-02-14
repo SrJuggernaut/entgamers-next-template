bf56585f9d5721f6bd553daa3c4cf5896ecd2ec1
SrJuggernaut <jugger@srjuggernaut.dev>
2024-02-14 15:14:04 -0600

    feat: skeleton & table ui components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,151 @@
+import Skeleton from '@/components/ui/Skeleton'
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableHeadCell, TableRow } from '@/components/ui/Table'
 import Typography from '@/components/ui/Typography'
-import { faCheck } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { type FC } from 'react'
 
 const Home: FC = () => {
   return (
     <>
       <Typography variant="h1" align="center" >EntGamers Next Template</Typography>
-      <Typography variant="body1">A template for EntGamers websites</Typography>
-      <ul className="fa-ul">
-        <li>
-          <FontAwesomeIcon icon={faCheck} listItem /> Cosas
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faCheck} listItem /> Cosas
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faCheck} listItem /> Cosas
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faCheck} listItem /> Cosas
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faCheck} listItem /> Cosas
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faCheck} listItem /> Cosas
-        </li>
-      </ul>
+      <Skeleton />
+      <TableContainer
+      >
+        <Table
+        >
+          <TableHead>
+            <TableRow>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+              <TableHeadCell>Header</TableHeadCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+              <TableCell>Cell</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   )
 }
